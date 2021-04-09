cc57e501b00131586c7d75f418e00f2e3c226dd5
TitanV <a01098930999@gmail.com>
2021-04-09 12:04:39 +0900

    add - process

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as WebSocket from 'ws';
 import rclient from 'redis-async';
 import dotenv from 'dotenv';
 import {verify} from './lib/command';
+import {process} from './lib/process';
 dotenv.config();
 
 const wss = new WebSocket.Server({ port: 3001 });
@@ -23,17 +24,18 @@ wss.on('connection', function connection(ws, req) {
 
 wss.on('connection', (ws: WebSocket, req : any) => {
 
-    ws.on('message', async (message: string) => {
-      await rclient.get("vjs06nx4swo@liamg.moc", async (err, reply) => {
-        console.log('received: %s', await verify(message));
-      });
+  ws.on('message', async (message: string) => {
+    message = await verify(message);
+    console.log('received: %s', await verify(message));
+    process(message);
 
-      wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send("brodcast: "+message);
-        }
-      });
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send("brodcast: "+message);
+        
+      }
     });
+  });
 
     //send immediatly a feedback to the incoming connection    
     ws.send('Hi there, I am a WebSocket server');
---
+++ src/lib/process.ts
@@ -0,0 +1,35 @@
+import rclient from 'redis-async';
+
+
+export const process = (async (str) => {
+
+  switch (str[0]) {
+    case 'create':
+      rclient.hmset(str[2],{
+        "hp":100,
+        "x":str[3],
+        "y":str[4]
+      });
+      break;
+    case 'move':
+      rclient.hmset(str[2],{
+        "x":str[3],
+        "y":str[4]
+      });
+      break;
+    case 'attack':
+      break;
+    case 'damage':
+      break;
+    case 'ohmygod':
+      break;
+    case 'lv999boss':
+      break;
+  }
+
+
+
+
+
+  return str.split(',');
+});
